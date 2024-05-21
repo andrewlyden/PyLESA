5b88a37cb2f99e1ccec475a56fbff575f5f9413b
thisistheplace <beancannell@gmail.com>
2024-05-21 07:31:13 +0100

    Removed old tests

--- tests/test_read_excel.py
+++
@@ -1,7 +0,0 @@
-import pylesa.io.read_excel as read_excel
-
-read_excel.read_inputs('WWHC_FOC_WM.xlsx')
-
-# myInputs = read_excel.Input('master.xlsx')
-# myInputs.parametric_analysis()
-# print(myInputs.container)
