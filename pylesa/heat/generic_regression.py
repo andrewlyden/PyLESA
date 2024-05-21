cbd5a93bda628922a347fd9e72da4644a174709a
thisistheplace <beancannell@gmail.com>
2024-05-21 08:52:06 +0100

    Refactored regression class to become function and added testing

--- pylesa/heat/generic_regression.py
+++ pylesa/heat/generic_regression.py
@@ -15,13 +15,7 @@ def cop(pump: HP, flow_temp: float, ambient_temp: float) -> float:
                 0.121 * (flow_temp - ambient_temp) +
                 0.00063 * (flow_temp - ambient_temp) ** 2
             )
-        case HP.GSHP:
-            return (8.77 -
-                0.15 * (flow_temp - ambient_temp) +
-                0.000734 * (flow_temp - ambient_temp) ** 2
-            )
-        case HP.WSHP:
-            # Same as GSHPs
+        case HP.GSHP | HP.WSHP:
             return (8.77 -
                 0.15 * (flow_temp - ambient_temp) +
                 0.000734 * (flow_temp - ambient_temp) ** 2
@@ -37,11 +31,11 @@ def duty(pump: HP, ambient_temp: float) -> float:
             return (5.80 +
                 0.21 * (ambient_temp)
             )
-        case HP.GSHP:
+        case HP.GSHP | HP.WSHP:
             return (9.37 +
                 0.30 * ambient_temp
             )
         case _:
-            msg = f"Cannot calculate cop for heatpump {pump}, must be one of {[HP.ASHP, HP.GSHP]}"
+            msg = f"Cannot calculate cop for heatpump {pump}, must be one of {[HP.ASHP, HP.GSHP, HP.WSHP]}"
             LOG.error(msg)
             raise KeyError(msg)
